b4eec1161a1d3f7b3a81f88e8386c209d4407dd7
Ozsie <oscar@djupfeldt.se>
2017-10-16 19:30:33 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <tbody>
+      <tr v-for="test in tests">
+        <td>{{test}}</a></td>
+      </tr>
+    </tbody>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Firebase from 'firebase'
+let config = {
+  apiKey: 'AIzaSyCwWyGUQ--YykBTlM0iK1_e7i9fgpvHEJs',
+  authDomain: 'ia-spel.firebaseapp.com',
+  databaseURL: 'https://ia-spel.firebaseio.com',
+  projectId: 'ia-spel',
+  storageBucket: 'ia-spel.appspot.com',
+  messagingSenderId: '442991775376'
+}
+
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let testRef = db.ref('test')
+
+export default {
+  name: 'app',
+  firebase: {
+    tests: testRef
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
